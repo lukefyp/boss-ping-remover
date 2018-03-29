667770509cd85afa6cbd35bd914bb0c5b7550a2b
mister-kay <mister-kay@github.com>
2018-03-29 13:43:55 -0500

    rewrite part 2

---
+++ README.md
@@ -0,0 +1,4 @@
+# Boss Ping Remover
+Makes the bosses attacks match up with servers
+
+Command(s): `BPR` -- Enable/Disable the module
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require('fs'),
     path = require('path'),
-    Ping = require('./ping.js')
+    Ping = require('./ping.js'),
+    Command = require('command')
 
 module.exports = function getBossSkills(dispatch) {
     // constants
     const ping = Ping(dispatch),
+        command = Command(dispatch)
         config = require('./config.json')
 
     // variables
@@ -14,9 +16,26 @@ module.exports = function getBossSkills(dispatch) {
         currentActions = {},
         reading = [],
         writing = []
+
+    // commands
+    command.add('bpr', (arg) => {
+        if (arg && arg.toLowerCase() == 'debug') config.debug = !config.debug
+        else {
+            config.enabled = !config.enabled
+            command.message(`Boss Ping Remover ${config.enabled ? 'enabled' : 'disabled'}.`)
+            if (!config.enabled) {
+                writeCache(cache)
+                zone = -1
+                cache = {}
+                mobs = {}
+                currentActions = {}
+            }
+        }
+    })
     
     // async write for performance
     function writeCache(cache) {
+        clean(cache)
         for (let huntingZoneId in cache) {
             // if being written, don't retry
             if (!writing.includes(huntingZoneId)) {
@@ -24,7 +43,22 @@ module.exports = function getBossSkills(dispatch) {
                 fs.writeFile(path.join(__dirname, 'data', `${huntingZoneId}.json`), JSON.stringify(cache[huntingZoneId], null, '\t'), (err) => {
                     writing.splice(writing.indexOf(huntingZoneId), 1)
                     if (err) return
+                    if (config.debug) console.log(`data/${huntingZoneId}.json written`)
                 })
+            }
+        }
+    }
+
+    // delete empty objects inside an object
+    function clean(obj) {
+        for (let key in obj) {
+            if (obj[key] && typeof obj[key] === "object") {
+                if (Object.keys(obj[key]).length === 0) {
+                    delete obj[key]
+                }
+                else {
+                    clean(obj[key])
+                }
             }
         }
     }
@@ -39,6 +73,7 @@ module.exports = function getBossSkills(dispatch) {
                     reading.splice(reading.indexOf(huntingZoneId), 1)
                     if (err) return
                     Object.assign(cache[huntingZoneId], JSON.parse(data))
+                    if (config.debug) console.log(`data/${huntingZoneId}.json read`)
                 })
             }
             // if being written, try again later
@@ -49,35 +84,41 @@ module.exports = function getBossSkills(dispatch) {
     }
 
     // S_SPAWN_NPC
-    dispatch.hook('S_SPAWN_NPC', 6, event => {
-        let mobId = JSON.stringify(event.gameId),
-            huntingZoneId = event.huntingZoneId,
-            templateId = event.templateId
-        mobs[mobId] = huntingZoneId
-        // if not cached, try to read from data folder
-        if (!cache[huntingZoneId]) {
-            cache[huntingZoneId] = {}
-            readData(huntingZoneId)
+    dispatch.hook('S_SPAWN_NPC', 6, {filter: {fake: null}}, event => {
+        if (config.enabled) {
+            let mobId = JSON.stringify(event.gameId),
+                huntingZoneId = event.huntingZoneId,
+                templateId = event.templateId
+            mobs[mobId] = huntingZoneId
+            // if not cached, try to read from data folder
+            if (!cache[huntingZoneId]) {
+                cache[huntingZoneId] = {}
+                readData(huntingZoneId)
+            }
+            if (!cache[huntingZoneId][templateId]) cache[huntingZoneId][templateId] = {}
         }
-        if (!cache[huntingZoneId][templateId]) cache[huntingZoneId][templateId] = {}
     })
 
     // S_DESPAWN_NPC
-    dispatch.hook('S_DESPAWN_NPC', 3, event => {
-        let mobId = JSON.stringify(event.gameId)
-        if (mobs[mobId]) delete mobs[mobId]
-        if (currentActions[mobId]) delete currentActions[mobId]
+    dispatch.hook('S_DESPAWN_NPC', 3, {filter: {fake: null}}, event => {
+        if (config.enabled) {
+            let mobId = JSON.stringify(event.gameId)
+            if (mobs[mobId]) delete mobs[mobId]
+            if (currentActions[mobId]) delete currentActions[mobId]
+        }
     })
 
     // S_LOAD_TOPO
     dispatch.hook('S_LOAD_TOPO', 3, event => {
-        if (zone != event.zone) {
-            writeCache(cache)
-            cache = {}
+        if (config.enabled) {
+            if (zone != event.zone) {
+                writeCache(cache)
+                cache = {}
+            }
+            zone = event.zone
+            mobs = {}
+            currentActions = {}
         }
-        zone = event.zone
-        mobs = []
-        currentActions = []
     })
 
     // S_ACTION_STAGE
@@ -87,9 +128,8 @@ module.exports = function getBossSkills(dispatch) {
             templateId = event.templateId,
             skill = parseInt('0x' + event.skill.toString(16).slice(-4))
         if (huntingZoneId) {
-            if (config.debug) console.log(`sActionStage <- ${huntingZoneId} ${templateId} ${skill} ${event.stage}`)
-            if (currentActions[mobId] && event.stage > currentActions[mobId].stage) {
-                //if (config.debug) console.log(huntingZoneId, templateId, skill, JSON.stringify(currentActions[mobId]), Date.now())
+            // if multi stage, do not update start time
+            if (currentActions[mobId] && event.id == currentActions[mobId].id && event.stage > currentActions[mobId].stage) {
                 currentActions[mobId] = {
                     time: currentActions[mobId].time,
                     speed: event.speed,
@@ -109,9 +149,12 @@ module.exports = function getBossSkills(dispatch) {
             let length = cache[huntingZoneId][templateId][skill]
             if (length > 0) {
                 // shorten by ping
+                if (config.debug) console.log(`[${Date.now().toString().slice(-4)}] <* sActionStage ${huntingZoneId} ${templateId} ${skill}` 
+                    + `s${event.stage} bpr${Math.min(ping.avg,Math.floor(length/event.speed-1000/config.minCombatFPS))}`)
                 event.speed = event.speed * length / Math.max(length - ping.avg * event.speed, 1000/config.minCombatFPS)
                 return true
             }
+            if (config.debug) console.log(`[${Date.now().toString().slice(-4)}] <- sActionStage ${huntingZoneId} ${templateId} ${skill} s${event.stage}`)
         }
     })
 
@@ -124,18 +167,16 @@ module.exports = function getBossSkills(dispatch) {
         if (huntingZoneId && currentActions[mobId] && currentActions[mobId].id == event.id) {
             let time = (Date.now() - currentActions[mobId].time) / currentActions[mobId].speed
             delete currentActions[mobId]
+            if (config.debug) console.log(`[${Date.now().toString().slice(-4)}] <- sActionEnd ${huntingZoneId} ${templateId} ${skill} t${event.type} ${time}ms`)
             if (event.type == 0) {
                 if (!cache[huntingZoneId][templateId]) cache[huntingZoneId][templateId] = {}
                 cache[huntingZoneId][templateId][skill] = cache[huntingZoneId][templateId][skill] ? (cache[huntingZoneId][templateId][skill] + time) / 2 : time
-            }
-            else {
-                if (config.debug) console.log(huntingZoneId, templateId, skill, event.type, time)
             }
         }
     })
 
     // S_EXIT
-    dispatch.hook('S_EXIT', 'raw', ()=>{
-        writeCache(cache)
+    dispatch.hook('S_EXIT', 'raw', {order: -999, filter: {fake: null}}, () => {
+        if (config.enabled) writeCache(cache)
     })
 }
