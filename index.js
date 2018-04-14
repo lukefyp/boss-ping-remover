2ac5d9e8bb8a3aaaf066e248c1758af5be6cdf91
Mister-Kay <private-information>
2018-04-14 17:13:07 -0500

    remove redundant code and fix in-game ping reader

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const bpr = require('./lib/bpr.js')
 module.exports = function BPR(dispatch) {
-    dispatch.hookOnce('C_CHECK_VERSION', 'raw', () => {
-        bpr(dispatch)
-    })
+    bpr(dispatch)
 }
