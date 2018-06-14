ecad5883553eb5fdb15a131182f7f25dcf60a4ec
Mister-Kay <private-information>
2018-06-14 08:37:42 -0500

    KTera update Unsupported

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const bpr = require('./lib/bpr.js')
 module.exports = function BPR(dispatch) {
+    if (dispatch.base.majorPatchVersion >= 74) {
+        console.log('boss-ping-remover - KTera definitions unsupported')
+        return
+    }
     bpr(dispatch)
 }
