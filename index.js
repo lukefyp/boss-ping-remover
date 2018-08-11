5e565f77d5d865dca48c7abb195697f976b40170
Mister-Kay <private-information>
2018-08-11 14:46:24 -0500

    update protocol

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const bpr = require('./lib/bpr.js')
 module.exports = function BPR(dispatch) {
+    /*
     if (dispatch.base.majorPatchVersion >= 74) {
         console.log('boss-ping-remover - KTera definitions unsupported')
         return
     }
+    */
     bpr(dispatch)
 }
