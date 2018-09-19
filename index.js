11aaccc2b4503d49064031febdf3c977cdcf5470
Mister-Kay <private-information>
2018-09-18 20:57:03 -0500

    rewrite/fix mob tracking/fix exit data writing

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const bpr = require('./lib/bpr.js')
 module.exports = function BPR(dispatch) {
-    /*
-    if (dispatch.base.majorPatchVersion >= 74) {
-        console.log('boss-ping-remover - KTera definitions unsupported')
-        return
-    }
-    */
-    bpr(dispatch)
+    bpr(dispatch, this)
 }
